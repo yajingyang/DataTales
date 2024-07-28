adb1f9b28e739ccf8e496a52a05f4d7b77078be6
Yang Yajing <yajingyang@outlook.com>
2024-07-28 16:57:48 +0800

    Update reasoning_evaluation.py

--- reasoning_evaluation.py
+++ reasoning_evaluation.py
@@ -4,7 +4,6 @@ import time
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "1,3"
-os.environ['TRANSFORMERS_CACHE'] = "/mnt/data/yajing/.cache/huggingface/hub"
 import openai
 
 from tqdm import tqdm
